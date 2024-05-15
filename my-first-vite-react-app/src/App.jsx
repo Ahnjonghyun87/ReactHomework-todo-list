1dac7dc28f560f84314f259468d818fa3c4f05a1
[Github <helgo1318@gmail.com>
2024-05-15 23:22:40 +0900

    feat.목업 후 함수구현중-setTitle인식안되는중

--- my-first-vite-react-app/src/App.jsx
+++ my-first-vite-react-app/src/App.jsx
@@ -1,6 +1,22 @@
 import './App.css';
 
 const App = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [todos,setTodos] = useState([]);
+
+const onClick = () => {
+  
+  setTodos ([title, description]);
+
+  const onChangeTitle = (event) => {
+    setTitle{event.target.value};
+  };
+  const onChangeDescription = (event) => {
+    setDescription{event.target.value};
+  };
+};
+
 return (
   <div className="App">
     <div className="header">
@@ -9,9 +25,19 @@ return (
     </div>
   
   <div className="form-container">
-    <input type="text" placeholder="제목" />
-    <input type="text" placeholder="내용" />
-    <button>추가하기</button>
+    <input
+      type="text"
+      placeholder="제목"
+      onChange={onChangeTitle}
+      value={title}
+     />
+    <input
+      type="text"
+      placeholder="내용" 
+      onChange={onChangeDescription}
+      value={description}
+      />
+    <button onClick={onClick}>추가하기</button>
   </div>
   <div className="progress-container">
     <div>working..🔥</div>
@@ -20,7 +46,10 @@ return (
     <div>done..!🏆</div>
   </div>
   </div>
-)
+);
+
+
+
 };
 
 export default App;

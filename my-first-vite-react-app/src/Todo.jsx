782cdf86a6c97ff0aeabf7852ae88f62ac8f0b93
[Github <helgo1318@gmail.com>
2024-05-16 13:25:39 +0900

    feat/버튼overflow시 찐따나는 부분 수정

--- my-first-vite-react-app/src/Todo.jsx
+++ my-first-vite-react-app/src/Todo.jsx
@@ -10,10 +10,12 @@ const Todo = ({
     <li className="todoCard">
       <div>{title}</div>
       <div>{description}</div>
-      <button onClick={() => onDeleteTodo(id)}>삭제</button>
-      <button onClick={() => onChangeTodoState(id)}>
-        {isDone ? "취소" : "완료"}
-      </button>
+      <div className="button-container">
+        <button onClick={() => onDeleteTodo(id)}>삭제</button>
+        <button onClick={() => onChangeTodoState(id)}>
+          {isDone ? "취소" : "완료"}
+        </button>
+      </div>
     </li>
   );
 };
